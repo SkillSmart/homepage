7645fe17eaf53d7b63f9d3a615801573fe279b87
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2020-03-02 08:35:34 +0100

    feat: refactored folders for react pages structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Aux from 'react-aux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {LandingPage} from './pages';
+
+
+
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={LandingPage}/>
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
